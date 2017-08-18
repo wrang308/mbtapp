4386acb9b787f4e81bd5aab24f6a04b143cb7e16
wrang308 <gusmarwr@student.gu.se>
2017-08-18 08:50:48 +0200

    added text to some lekar

--- app/src/main/java/com/example/marku/mbtapp/lekar/Text_lekar/Text_fruktsallad.java
+++ app/src/main/java/com/example/marku/mbtapp/lekar/Text_lekar/Text_fruktsallad.java
@@ -6,7 +6,14 @@ package com.example.marku.mbtapp.lekar.Text_lekar;
 
 public class Text_fruktsallad {
 
-    private String text_fruktsallad = "TODO";
+    private String text_fruktsallad = "Deltagarna sätter sig på stolar i en ring. En deltagare är " +
+            "ledare och ställer sig i mitten. De andra är frukter; några är äpplen, några päron, " +
+            "några bananer osv. Leken börjar och ledaren ropar ?Alla äpplen byter plats?. Då ska " +
+            "alla äpplen resa sig och byta plats samtidigt som ledaren försöker sätta sig på en " +
+            "stol. Om ledaren ropar ?fruktsallad!? ska alla resa sig och byta plats. Om ledaren " +
+            "lyckas sätta sig på en stol får den som blev utan vara nästa ledare\n" +
+            "\n" +
+            "Man kan också lägga till hopp, då ska man hoppa till platsen till vänster om en fall den är ledig.\n";
 
     public String getText_fruktsallad() {
         return text_fruktsallad;
